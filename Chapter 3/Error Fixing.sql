16850f507a1925ee179a2be6b2817b030998e75d
mihransimonian <53622594+mihransimonian@users.noreply.github.com>
2021-07-13 20:25:08 +0200

    Create Error Fixing.sql
    Fixes the error which occurs when opening the Database Overview Diagram. Not covered in course, so might be required for the other classes as well.

---
+++ Chapter 3/Error Fixing.sql
@@ -0,0 +1,5 @@
+-- Fix error which pops up when viewing the Database Diagrams error, code 15517
+USE [Chapter 3 - Sales (Keyed)]
+GO
+ALTER AUTHORIZATION ON DATABASE::[Chapter 3 - Sales (Keyed)] TO [sa]
+GO
